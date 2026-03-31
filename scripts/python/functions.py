a5d389a827f4798188db84e2f26e790f59da14e0
schauer <linus.schauer@posteo.de>
2026-03-31 14:45:13 +0200

    maintain imports for R

--- scripts/python/functions.py
+++ scripts/python/functions.py
@@ -2,19 +2,12 @@
 """
 Stochastic Landscape Model (SLM) - Core Functions
 
-Backward-compatible re-export module. Import from stochastic_model or routing
-directly for new code.
-
-This script contains:
-(1) Subcatchment-scale stochastic model of Musolff et al. (2017) (https://doi.org/10.1002/2017GL072630)
-(2) MCT routing algorithm (https://doi.org/10.5194/hess-11-1645-2007)
-
 Author: schauer
 Created: Fri Feb 23 09:40:07 2024
 """
 
-from stochastic_small_scale_model import (
-    subcatchment_scale_module,
+from stochastic_headwater_model import (
+    stochastic_headwater_model,
     calculate_cumulative_rainfall_for_event,
     calculate_event_mass_fractions,
     SPIN_UP_YEARS,
@@ -26,6 +19,7 @@ from routing import (
     reach_routing,
     reach_routing_single_time_step,
     resample_routed_lists,
+    compute_hydraulic_properties,
     stage_discharge_relationship,
     d_stage_discharge_relationship,
     newton_raphson,
@@ -33,4 +27,6 @@ from routing import (
     COURANT_NUMBER_THRESHOLD_HIGH,
     COURANT_NUMBER_THRESHOLD_LOW,
     MAX_ITER,
+    SECONDS_PER_DAY,
+    INFLOW_RATIO_THRESHOLD,
 )
